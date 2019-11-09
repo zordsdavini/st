ef7973ad4bcbf99359e0b3c8fd72b26cb7e65042
Arnas Udovicius <arnas.u@estina.lt>
2019-11-09 23:11:35 +0200

    set cursor to red

--- config.h
+++ config.h
@@ -118,7 +118,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 1;
 static unsigned int defaultrcs = 257;
 
 /*
